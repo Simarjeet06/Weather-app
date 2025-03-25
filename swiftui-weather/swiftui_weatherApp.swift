43a157410c8107b9fc11f4b8de97e5bda4a0e611
Simarjeet Kaur <simarjeetkaur6603@gmail.com>
2025-03-26 00:24:07 +0530

    Initial Commit

---
+++ swiftui-weather/swiftui_weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_weatherApp.swift
+//  swiftui-weather
+//
+//  Created by Simarjeet Kaur on 26/03/25.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
